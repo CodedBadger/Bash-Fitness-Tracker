f2981f6f2f8f6680466440d47162c19182bb765c
CodedBadger <arkbuysthings@gmail.com>
2025-09-27 14:56:56 -0400

    add functionslib2

--- attempt2.sh
+++ attempt2.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
+maxSQT=470; SQT11=
+maxBN=315
+maxDL=565
 
-weigh1=320
-weigh2=350
-weigh3=375
+
+
+
+i=0
+I=" $i"
 echo "
 +=============================================================================================+
-|Cycle 1 | sets x reps | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday |
+|Cycle$I | sets x reps | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday |
 |========+=============+========+=========+===========+==========+========+===================|
-|Week  1 |  1   x  5   | $SQT11 | Cardio  | $BN11     |  Cardio  | $DL11  | Back Day | Chest  |
-|        |  1   x  5   | $SQT12 | Cardio  | $BN12     |  Cardio  | $DL12  | Back Day | Chest  |
-|        |  1   x  5+  | $SQT13 | Cardio  | $BN13     |  Cardio  | $DL13  | Back Day | Chest  |
+|Week  1 |  1   x  5   | $SQT111| Cardio  |  $BN111   |  Cardio  | $DL111 | Back Day | Chest  |
+|        |  1   x  5   | $SQT112| Cardio  |  $BN112   |  Cardio  | $DL112 | Back Day | Chest  |
+|        |  1   x  5+  | $SQT113| Cardio  |  $BN113   |  Cardio  | $DL113 | Back Day | Chest  |
+| Acc    |  4   x  10  | $DLACC | Cardio  |   $ROW    |  Cardio  | $SQACC | Back Day | Chest  |
 |========+=============+========+=========+===========+==========+========+===================|
-|Week  2 |  1   x  3   | $SQT21 | Cardio  | $BN21     |  Cardio  | $DL21  | Back Day | Chest  |
-|        |  1   x  3   | $SQT22 | Cardio  | $BN22     |  Cardio  | $DL22  | Back Day | Chest  |
-|        |  1   x  3+  | $SQT23 | Cardio  | $BN23     |  Cardio  | $DL23  | Back Day | Chest  |
+|Week  2 |  1   x  3   | $SQT21 | Cardio  |   $BN21   |  Cardio  | $DL21  | Back Day | Chest  |
+|        |  1   x  3   | $SQT22 | Cardio  |   $BN22   |  Cardio  | $DL22  | Back Day | Chest  |
+|        |  1   x  3+  | $SQT23 | Cardio  |   $BN23   |  Cardio  | $DL23  | Back Day | Chest  |
+| Acc    |  4   x  10  | $DLACC | Cardio  |   $ROW    |  Cardio  | $SQACC | Back Day | Chest  |
 |========+=============+========+=========+===========+==========+========+===================|
-|Week  3 |  1   x  5   | $SQT31 | Cardio  | $BN31     |  Cardio  | $DL31  | Back Day | Chest  |
-|        |  1   x  3   | $SQT32 | Cardio  | $BN32     |  Cardio  | $DL32  | Back Day | Chest  |
-|        |  1   x  1+  | $SQT33 | Cardio  | $BN33     |  Cardio  | $DL33  | Back Day | Chest  |
+|Week  3 |  1   x  5   | $SQT31 | Cardio  |   $BN31   |  Cardio  | $DL31  | Back Day | Chest  |
+|        |  1   x  3   | $SQT32 | Cardio  |   $BN32   |  Cardio  | $DL32  | Back Day | Chest  |
+|        |  1   x  1+  | $SQT33 | Cardio  |   $BN33   |  Cardio  | $DL33  | Back Day | Chest  |
+| Acc    |  4   x  10  | $DLACC | Cardio  |   $ROW    |  Cardio  | $SQACC | Back Day | Chest  |
 |========+=============+========+=========+===========+==========+========+==========+========+
-|Week  4 |
+|Week  4 |  4   x  10  | $SQDL4 | Cardio  |   $BNDL4  |  Cardio  | $DLDL4 | Back Day | Chest  |
+| Acc    |  4   x  10  | $DLACC | Cardio  |   $ROW    |  Cardio  | $SQACC | Back Day | Chest  |
+|========+=============+========+=========+===========+==========+========+==========+========+
 "
